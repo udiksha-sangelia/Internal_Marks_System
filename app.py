1c62f354a71a27bbd3f42e4ff68680108dde1908
udiksha <us041820@gmail.com>
2026-07-03 12:19:33 +0530

    config made

--- app.py
+++ app.py
@@ -913,7 +913,7 @@ def faculty_logout():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(
         host="0.0.0.0",
         port=port,
